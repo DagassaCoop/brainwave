30ddb91f25dfd5e37ce2d7c5a4fb28e77323be26
dagas_sa <dagassacoop@gmail.com>
2024-11-10 17:45:04 -0500

    add project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+
+// Assets
 import "./index.css";
-import App from "./App.tsx";
+
+// Router
+import router from "./app/router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 );
