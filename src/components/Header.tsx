c4c0c9ed307c916825fa57fa6a928c2636a2033d
dagas_sa <dagassacoop@gmail.com>
2024-11-11 16:27:27 -0500

    update: header animation with gsap

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { useLocation } from "react-router-dom";
-import { MouseEvent, useState } from "react";
+import { MouseEvent, useRef, useState } from "react";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 // Assets
 import { brainwave } from "@/assets";
@@ -13,42 +15,54 @@ import Button from "./Button";
 
 const Header = () => {
   const location = useLocation();
-  const [openNavigation, setOpenNavigation] = useState(true);
+  const [openNavigation, setOpenNavigation] = useState(false);
 
+  const openNavigationMove = useRef<gsap.core.Tween | undefined>(undefined);
+
+  useGSAP(() => {
+    const navigation = document.getElementById("navigation");
+    if (!navigation) return;
+
+    gsap.set(navigation, { y: "-100vh" });
+    openNavigationMove.current = gsap.to(navigation, {
+      y: 0,
+      duration: 0.5,
+      ease: "none",
+      paused: true,
+    });
+  });
   const toggleOpenNavigation = (e: MouseEvent) => {
     e.stopPropagation();
 
     if (openNavigation) {
       enablePageScroll();
       setOpenNavigation(false);
+      openNavigationMove?.current?.timeScale(2).reverse();
     } else {
       disablePageScroll();
       setOpenNavigation(true);
+      openNavigationMove?.current?.play();
     }
   };
 
   const handleClick = () => {
     enablePageScroll();
     setOpenNavigation(false);
+    openNavigationMove?.current?.timeScale(2).reverse();
   };
 
   return (
     <div
-      className={[
-        "fixed top-0 left-0 z-50 w-full border-b border-n-6 lg:bg-n-8/90",
-        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm",
-      ].join(" ")}
+      className="fixed top-0 left-0 z-50 w-full border-b border-n-6 bg-n-8 lg:bg-n-8/90"
       onClick={handleClick}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a href="#hero" className="block w-[12rem] xl:mr-8">
+        <a href="#hero" className="block z-3 w-[12rem] xl:mr-8">
           <img src={brainwave} alt="Brainwave" width={190} height={40} />
         </a>
         <nav
-          className={[
-            "fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent",
-            openNavigation ? "flex" : "hidden",
-          ].join(" ")}
+          id="navigation"
+          className="flex fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent"
         >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((n) => {
